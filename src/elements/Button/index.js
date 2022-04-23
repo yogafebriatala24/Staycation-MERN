98773ab0bdb56e81e4d5c0646d94b780a2bed8c7
Yoga Febriatala <yogafebriatala24@gmail.com>
2022-04-23 17:05:50 +0700

    Update

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -30,15 +30,15 @@ export default function Button(props) {
     );
   }
 
-  if (props.type == "link") {
+  if (props.type === "link") {
     if (props.isExternal) {
       return (
         <a
           href={props.href}
           className={className.join(" ")}
           style={props.style}
-          target={props.target == "_blank" ? "_blank" : undefined}
-          rel={props.target == "_blank" ? "noopener noreferrer" : undefined}
+          target={(props.target = "_blank" ? "_blank" : undefined)}
+          rel={(props.target = "_blank" ? "noreferrer" : undefined)}
         >
           {props.children}
         </a>
